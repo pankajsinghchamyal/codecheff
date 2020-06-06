837eb14a4f46d12888dd8b400fa8ae6e8cdbedf5
pankaj singh chamyal <pankajsinghchamyal569@gmail.com>
2020-06-06 22:48:17 +0530

    my python coding templete

---
+++ templete_python.py
@@ -0,0 +1,14 @@
+
+
+
+# for taking the input with space separated value :-
+
+k =list(map(int,input("give:").split()))
+
+
+# input output using stdin by reading line :-
+
+from sys import stdin, stdout
+
+n,m,k=map(int,stdin.readline().split())
+
